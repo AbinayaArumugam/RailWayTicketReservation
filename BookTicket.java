5283656df168e56cceee664ae0e1acd849d4503c
AbinayaArumugam <47531319+AbinayaArumugam@users.noreply.github.com>
2021-09-28 18:42:49 +0530

    Update BookTicket.java

--- BookTicket.java
+++ BookTicket.java
@@ -4,56 +4,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BookTicket {
-    static Map<Integer,Passenger> passenger=new HashMap<>();
-    public static void bookTicket(Passenger p,int berthInfo,String allotedBerth){
-      p.setNumber(berthInfo);
-      p.setAlloted(allotedBerth);
-      passenger.put(p.getPassengerId(),p);
-      TicketBooker.bookedTicket.add(p.getPassengerId());
+
+    public static void bookTicket(Passenger passenger,int berthInfo,String allotedBerth){
+      passenger.setNumber(berthInfo);
+      passenger.setAlloted(allotedBerth);
+      Runner.map.put(passenger.getTicketId(),passenger);
+      TicketBooker.bookedTicket.add(passenger.getPassengerId());
         System.out.println("--------------Booked Successfully");
     }
-    public static void addToRAC(Passenger p,int racInfo,String allotedRAC){
-        p.setNumber(racInfo);
-        p.setAlloted(allotedRAC);
-        passenger.put(p.getPassengerId(),p);
-        TicketBooker.racList.add(p.getPassengerId());
+    public static void addToRAC(Passenger passenger,int racInfo,String allotedRAC){
+        passenger.setNumber(racInfo);
+        passenger.setAlloted(allotedRAC);
+      Runner.map.put(passenger.getTicketId(),passenger);
+        TicketBooker.racList.add(passenger.getPassengerId());
         TicketBooker.racPosition.remove(0);
         TicketBooker.availableRACTicket--;
         System.out.println("--------------added to RAC Successfully");
     }
-    public static void addToWaitingList(Passenger p,int waitingListInfo,String allotedWaitingList){
-        p.setNumber(waitingListInfo);
-        p.setAlloted(allotedWaitingList);
-        passenger.put(p.getPassengerId(),p);
-        TicketBooker.waitingList.add(p.getPassengerId());
+    public static void addToWaitingList(Passenger passenger,int waitingListInfo,String allotedWaitingList){
+        passenger.setNumber(waitingListInfo);
+        passenger.setAlloted(allotedWaitingList);
+        Runner.map.put(passenger.getTicketId(),passenger);
+        TicketBooker.waitingList.add(passenger.getPassengerId());
         TicketBooker.waitingListPosition.remove(0);
         TicketBooker.availableWaitingList--;
         System.out.println("----------------added to Waiting List");
     }
     public static void cancelTicket(int id){
-        Passenger p=passenger.get(id);
-        passenger.remove(id);
-        int positionBooked=p.getNumber();
-        if(p.getAlloted().equals("L")){
+        Passenger passenger=Runner.map.get(id);
+        Runner.map.remove(id);
+        int positionBooked=passenger.getNumber();
+        if(passenger.getAlloted().equals("L")){
             TicketBooker.availableLowerBerth++;
             TicketBooker.lowerBerthPosition.add(positionBooked);
         }
-        else if(p.getAlloted().equals("M")){
+        else if(passenger.getAlloted().equals("M")){
             TicketBooker.availableMiddleBerth++;
             TicketBooker.middleBerthPosition.add(positionBooked);
         }
-        else if(p.getAlloted().equals("U")){
+        else if(passenger.getAlloted().equals("U")){
             TicketBooker.availableUpperBerth++;
             TicketBooker.upperBerthPosition.add(positionBooked);
         }
         if (TicketBooker.racList.size()>0) {
-            Passenger passengerFromRAC = BookTicket.passenger.get(TicketBooker.racList.poll());
+            Passenger passengerFromRAC = Runner.map.get(TicketBooker.racList.poll());
             int positionRAC = passengerFromRAC.getNumber();
            TicketBooker. racPosition.add(positionRAC);
             TicketBooker.racList.remove(passengerFromRAC.getPassengerId());
             TicketBooker.availableRACTicket++;
             if (TicketBooker.waitingList.size() > 0) {
-                Passenger passengerFromWaitingList = BookTicket.passenger.get(TicketBooker.waitingList.poll());
+                Passenger passengerFromWaitingList = Runner.map.get(TicketBooker.waitingList.poll());
                 int positionWL = passengerFromWaitingList.getNumber();
                TicketBooker. waitingListPosition.add(positionWL);
                 TicketBooker.waitingList.remove(passengerFromWaitingList.getPassengerId());
